6e00ba5bd1111bb7693d0bb2aba515cc934a6c4c
heleneshaikh <helene.shaikh@gmail.com>
2016-03-25 17:24:59 +0100

    add user and album entities

---
+++ ejb-module/src/main/java/com/realdolmen/course/domain/Album.java
@@ -0,0 +1,85 @@
+package com.realdolmen.course.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by heleneshaikh on 25/03/16.
+ */
+
+@Entity
+public class Album {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String title;
+    private String composer;
+    @Column(name="songs")
+    private int numberOfSongs;
+    @Enumerated(EnumType.STRING)
+    private Genre genre;
+    @ManyToOne
+    private User user;
+
+    public enum Genre {
+        ROCK, CLASSICAL, POP;
+    }
+
+    public Album() {
+    }
+
+    public Album(String title, String composer, int numberOfSongs, Genre genre) {
+        this.title = title;
+        this.composer = composer;
+        this.numberOfSongs = numberOfSongs;
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getComposer() {
+        return composer;
+    }
+
+    public void setComposer(String composer) {
+        this.composer = composer;
+    }
+
+    public int getNumberOfSongs() {
+        return numberOfSongs;
+    }
+
+    public void setNumberOfSongs(int numberOfSongs) {
+        this.numberOfSongs = numberOfSongs;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ ejb-module/src/main/java/com/realdolmen/course/domain/User.java
@@ -0,0 +1,73 @@
+package com.realdolmen.course.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by heleneshaikh on 25/03/16.
+ */
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(length = 50)
+    private String firstName;
+    @Column(length = 50, nullable = false)
+    private String lastName;
+    @Transient
+    private int age;
+    @OneToMany(mappedBy = "user")
+    List<Album> albumList;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public List<Album> getAlbumList() {
+        return albumList;
+    }
+
+    public void setAlbumList(List<Album> albumList) {
+        this.albumList = albumList;
+    }
+}
